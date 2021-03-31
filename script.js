95cde05bc3ff4d28ec8d614a8ed40628c3c0243b
steeve83 <58664522+steeve83@users.noreply.github.com>
2021-03-31 19:09:03 -0400

    new commit

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-Array.from(document.querySelectorAll(".navigation-button")).
+Array.from(document.querySelectorAll(".navigation-button"))
+.forEach(item=>{
+    item.onclick =()=>{item.parentElement.parentElement.classList.toggle("change");
+};
+});
